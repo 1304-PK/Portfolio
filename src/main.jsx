f03be740a115e9b06818c71b78372e041d15b730
PK-1304 <pushkark1304@gmail.com>
2026-03-12 22:04:07 +0530

    minor

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Portfolio.jsx'
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
