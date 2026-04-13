0bbf096954b679ba67eb74c4fef3944853791673
PK-1304 <pushkark1304@gmail.com>
2026-04-13 19:44:49 +0530

    minor changes

--- src/components/ui/orbiting-circles.jsx
+++ src/components/ui/orbiting-circles.jsx
@@ -22,7 +22,7 @@ export function OrbitingCircles({
           version="1.1"
           className="pointer-events-none absolute inset-0 size-full">
           <circle
-            className="stroke-black/10 stroke-1 dark:stroke-white/10"
+            className="stroke-white stroke-1"
             cx="50%"
             cy="50%"
             r={radius}
