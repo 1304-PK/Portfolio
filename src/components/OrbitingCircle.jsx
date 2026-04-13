d9e4a42616a4ed12a522b1b7d567dced13f7f11d
PK-1304 <pushkark1304@gmail.com>
2026-04-13 19:44:14 +0530

    change radius and stroke color of component

--- src/components/OrbitingCircle.jsx
+++ src/components/OrbitingCircle.jsx
@@ -1,17 +1,20 @@
 import { OrbitingCircles } from "@/components/ui/orbiting-circles"
 
-const OrbitingCircle = ({icons, iconSize, radius}) => {
+const OrbitingCircle = ({ skills, iconSize, radius }) => {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden">
-        <OrbitingCircles iconSize={iconSize} radius={radius} path={true} className={"stroke-amber-300"}>
-            {
-                icons.map(icon => {
-                    return(
-                        <img src={icon} alt="" />
-                    )
-                })
-            }
+    <div className="relative flex h-full w-full flex-1 flex-col items-center justify-center">
+      <div className="relative flex h-[560px] w-[560px] items-center justify-center overflow-visible p-8">
+        <OrbitingCircles iconSize={iconSize} radius={radius} path={true}>
+          {skills.map((skill) => {
+            return (
+              <div key={skill.title} className="skill-orbit-item group">
+                <span className="skill-orbit-tooltip">{skill.title}</span>
+                <img src={skill.image} alt={skill.title} />
+              </div>
+            )
+          })}
         </OrbitingCircles>
+      </div>
     </div>
   )
 }
